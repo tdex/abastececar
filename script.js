9a36c725ddf6d12b25307c4a2aff96faae08ef68
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2017-11-09 22:13:03 -0300

    Calculo de abastecimento

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var app = angular.module('myApp', []);
 app.controller('app', function ($scope) {
   var vm = $scope;
 
-  vm.precoGasolina = 0;
-  vm.consumoCarro = 0;
-  vm.distanciaViagem = 0;
+  vm.precoGasolina = '';
+  vm.consumoCarro = '';
+  vm.distanciaViagem = '';
 
   vm.calcularValorViagem = function () {
     var litrosNecessarios = vm.distanciaViagem / vm.consumoCarro;
