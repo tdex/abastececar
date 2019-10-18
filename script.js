8469751992b540d20ec792481512d5a5fcd0e7c7
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2019-10-18 14:56:33 -0300

    adição bt historico

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var app = angular.module('myApp', []);
-app.controller('app', function ($scope) {
+app.controller('app', ($scope) => {
   var vm = $scope;
 
   vm.precoGasolina = '';
   vm.consumoCarro = '';
   vm.distanciaViagem = '';
 
-  vm.calcularValorViagem = function () {
+  vm.calcularValorViagem = () => {
     var litrosNecessarios = vm.distanciaViagem / vm.consumoCarro;
 
     vm.valorViagem = vm.precoGasolina * litrosNecessarios;
   };
+
+  vm.salvarVeiculo = () => {
+
+  }
 });
